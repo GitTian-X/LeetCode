75b634e40d7383b67fabc9a590fc6f492ed302de
黄 <15111033824@163.com>
2021-02-26 09:51:31 +0800

    20210225：新增135、136、137、138、139五道题解

--- LeetCode/Solution.cs
+++ LeetCode/Solution.cs
@@ -3424,15 +3424,17 @@ namespace LeetCode
             public Node right;
             public Node next;
             public IList<Node> neighbors;
+            public Node random;
             public Node()
             {
                 val = 0;
                 neighbors = new List<Node>();
             }
-
             public Node(int _val)
             {
                 val = _val;
+                next = null;
+                random = null;
                 neighbors = new List<Node>();
             }
             public Node(int _val, List<Node> _neighbors)
@@ -4047,5 +4049,102 @@ namespace LeetCode
             }
             return spare < 0 ? -1 : (minIndex + 1) % len;
         }
+        public int Candy(int[] ratings)
+        {
+            int n = ratings.Length;
+            int ret = 1;
+            int inc = 1, dec = 0, pre = 1;
+            for (int i = 1; i < n; i++)
+            {
+                if (ratings[i] >= ratings[i - 1])
+                {
+                    dec = 0;
+                    pre = ratings[i] == ratings[i - 1] ? 1 : pre + 1;
+                    ret += pre;
+                    inc = pre;
+                }
+                else
+                {
+                    dec++;
+                    if (dec == inc)
+                    {
+                        dec++;
+                    }
+                    ret += dec;
+                    pre = 1;
+                }
+            }
+            return ret;
+        }
+        public int SingleNumber(int[] nums)
+        {
+            int res = 0;
+            foreach (var num in nums)
+            {
+                res ^= num;
+            }
+            return res;
+        }
+        public int SingleNumber1(int[] nums)
+        {
+            int seenOnce = 0, seenTwice = 0;
+            foreach (var num in nums)
+            {
+                seenOnce = ~seenTwice & (seenOnce ^ num);
+                seenTwice = ~seenOnce & (seenTwice ^ num);
+            }
+            return seenOnce;
+        }
+        public Node CopyRandomList(Node head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            Node ptr = head;
+            while (ptr != null)
+            {
+                Node newNode = new Node(ptr.val);
+                newNode.next = ptr.next;
+                ptr.next = newNode;
+                ptr = newNode.next;
+            }
+            ptr = head;
+            while (ptr != null)
+            {
+                ptr.next.random = ptr.random != null ? ptr.random.next : null;
+                ptr = ptr.next.next;
+            }
+            Node ptrOldList = head;
+            Node ptrNewList = head.next;
+            Node headOld = head.next;
+            while (ptrOldList != null)
+            {
+                ptrOldList.next = ptrOldList.next.next;
+                ptrNewList.next = ptrNewList.next != null ? ptrNewList.next.next : null;
+                ptrOldList = ptrOldList.next;
+                ptrNewList = ptrNewList.next;
+            }
+            return headOld;
+        }
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            var wordDictSet = new HashSet<string>(wordDict);
+            var dp = new bool[s.Length + 1];
+            dp[0] = true;
+            for (int i = 1; i <= s.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (dp[j] && wordDictSet.Contains(s.Substring(j, i - j)))
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[s.Length];
+        }
+        
     }
 }
