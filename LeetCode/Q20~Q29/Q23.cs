1b6ad8aae3a5824e26b5fa8736d50843402215b8
黄 <15111033824@163.com>
2021-04-07 18:22:53 +0800

    新增23重构题解

---
+++ LeetCode/Q20~Q29/Q23.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+
+namespace LeetCode.Q20_Q29
+{
+    class Q23
+    {
+        //给你一个链表数组，每个链表都已经按升序排列。
+        //请你将所有链表合并到一个升序链表中，返回合并后的链表。
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            ListNode MergeK(ListNode[] lists, int left, int right)
+            {
+                if (left == right) return lists[left];
+                if (left > right) return null;
+                int mid = (left + right) / 2;
+                return Merge2Lists(MergeK(lists, left, mid), MergeK(lists, mid + 1, right));
+            }
+            ListNode Merge2Lists(ListNode node1, ListNode node2)
+            {
+                ListNode dummy = new ListNode();
+                ListNode newHead = dummy;
+                while (node1 != null && node2 != null)
+                {
+                    if (node1.val <= node2.val)
+                    {
+                        dummy.next = node1;
+                        dummy = dummy.next;
+                        node1 = node1.next;
+                    }
+                    else
+                    {
+                        dummy.next = node2;
+                        dummy = dummy.next;
+                        node2 = node2.next;
+                    }
+                }
+                //处理剩余的结点
+                while (node1 != null)
+                {
+                    dummy.next = node1;
+                    dummy = dummy.next;
+                    node1 = node1.next;
+                }
+                while (node2 != null)
+                {
+                    dummy.next = node2;
+                    dummy = dummy.next;
+                    node2 = node2.next;
+                }
+                return newHead.next;
+            }
+            return MergeK(lists, 0, lists.Length - 1);
+        }
+    }
+}
