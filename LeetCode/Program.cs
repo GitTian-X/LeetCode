531ed8d9724bd36ff81357673bda42178dd57bbb
黄 <15111033824@163.com>
2021-04-09 17:03:30 +0800

    新增26、27、28、重构题解

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,7 +8,10 @@ namespace LeetCode
         static void Main(string[] args)
         {
             Q18 q = new Q18();
-            Console.WriteLine(q.FourSum(new int[] {1,0,-1,0,-2,2}, 0));
+            Console.WriteLine(q.FourSum(new int[] { 1, 0, -1, 0, -2, 2 }, 0));
+            int a = 1, b = 2;
+            a = b + (b = a) * 0;
+            Console.WriteLine($"a:{a}     b:{b}");
         }
     }
 }
---
+++ LeetCode/Q20~Q29/Q28.cs
@@ -0,0 +1,66 @@
+﻿namespace LeetCode.Q20_Q29
+{
+    class Q28
+    {
+        //实现 strStr() 函数。
+        //给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置(从0开始)。如果不存在，则返回  -1。
+        public int StrStr(string haystack, string needle)
+        {
+            int sourceLen = haystack.Length;
+            int patLen = needle.Length;
+            int[] Next()
+            {
+                int[] next = new int[patLen];
+                next[0] = -1;
+                int k = -1;
+                for (int i = 1; i < patLen; i++)
+                {
+                    while (k != -1 && needle[k + 1] != needle[i])
+                    {
+                        k = next[k];
+                    }
+                    if (needle[k + 1] == needle[i])
+                    {
+                        k++;
+                    }
+                    next[i] = k;
+                }
+                return next;
+            }
+            int KMP()
+            {
+                int[] next = Next();
+                int j = 0;
+                for (int i = 0; i < sourceLen; i++)
+                {
+                    while (j > 0 && haystack[i] != needle[j])
+                    {
+                        j = next[j - 1] + 1;
+                        if (patLen - j + i > sourceLen)
+                        {
+                            return -1;
+                        }
+                    }
+                    if (haystack[i] == needle[j])
+                    {
+                        j++;
+                    }
+                    if (j == patLen)
+                    {
+                        return i - patLen + 1;
+                    }
+                }
+                return -1;
+            }
+            if (patLen == 0)
+            {
+                return 0;
+            }
+            if (sourceLen == 0)
+            {
+                return -1;
+            }
+            return KMP();
+        }
+    }
+}
